24c115e7ea63367d6b2c2fcc7b29d0048421aa36
Glenn Pickett <donnegas@gmail.com>
2021-08-26 00:30:46 -0400

    Express REST API with MongoDB and Mongoose Part 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,48 +1,96 @@
 const express = require('express');
+const { response } = require('../app');
 const campsiteRouter = express.Router();
+const Campsite = require('../models/campsite');
 
 campsiteRouter.route('/')
 //app.all('/campsites', (req, res, next) => {
-.all((req, res, next) => {
+/*.all((req, res, next) => {
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
     next();
 })
-.get((req, res) => {
-    res.end('Will send all campsites');
+.get((req, res) => {*/
+.get((req, res, next) => {
+    Campsite.find()
+    .then(campsites => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsites);
+    })
+    //res.end('Will send all campsites');
+    .catch(err => next(err));
 })
-.put( (req, res) => {
+.post( (req, res, next) => {
+    Campsite.create(req.body)
+    .then(campsite => {
+        console.log('Campsite created ', campsite);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err))
     res.end(`Will send all campsite: ${req.body.name} with description: ${req.body.description}`);
 })
-.post((req, res) => {
+.put((req, res) => {
     res.statusCode = 403;
-    res.end('Post not supported campsites');
+    res.end('Put not supported campsites');
 })
-.delete((req, res) => {
-    res.statusCode = 200;
-    res.end('Deleting campsites');
+.delete((req, res, next) => {
+    //res.statusCode = 200;
+    //res.end('Deleting campsites');
+    Campsite.deleteMany()
+    .then(response => {
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 campsiteRouter.route('/:campsitesId')
 //app.all('/campsites', (req, res, next) => {
-.all((req, res, next) => {
+/*.all((req, res, next) => {
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
     next();
+})*/
+.get((req, res, next) => {
+    //res.end('Will send all campsites');
+    Campsite.findById(req.params.campsitesId)
+    .then(campsite=> {
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err=> next(err));
 })
-.get((req, res) => {
-    res.end('Will send all campsites');
-})
-.put( (req, res) => {
-    res.end(`Will send all campsite: ${req.body.name} with description: ${req.body.description}`);
+.put( (req, res, next) => {
+    //res.end(`Will send all campsite: ${req.body.name} with description: ${req.body.description}`);
+    Campsite.findByIdAndUpdate(req.params.campsitesId, {
+        $set: req.body
+    },{ new: true})
+    .then(campsite => {
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err=> next(err));
 })
 .post((req, res) => {
     res.statusCode = 403;
     res.end('Post not supported campsites');
 })
-.delete((req, res) => {
-    res.statusCode = 200;
-    res.end('Deleting campsites');
+.delete((req, res, next) => {
+    //res.statusCode = 200;
+    //res.end('Deleting campsites');
+    Campsite.findByIdAndDelete(req.params.campsitesId)
+    .then(response => {
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err=> next(err));
 });
 
 
