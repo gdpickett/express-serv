2a65d9355e3b28988a54cd14eaf8bf56285f19c4
Glenn Pickett <donnegas@gmail.com>
2021-09-26 00:54:29 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '123456-00-bnbnb',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '117122277286093',
+        clientSecret: '029aa5f1d0080ecb30e0867c60be88f5'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -87,4 +87,17 @@ router.get('/logout', cors.corsWithOptions, (req, res, next) => {
 	}
 })
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+	if(req.user){
+		const token = authenticate.getToken({_id: req.user._id});
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({success: true, token: token, status: 'Facebook login successful!'});
+		//return done(err, false);
+	}
+	if(!err && user) {
+		return done(null, user);
+	}
+});
+
 module.exports = router;
