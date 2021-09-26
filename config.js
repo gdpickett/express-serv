5dcf176623b94c74e92d481cb2e2dae905a60a82
Glenn Pickett <donnegas@gmail.com>
2021-09-26 01:20:16 -0400

    Mini Fix 2

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '123456-00-bnbnb',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '117122277286093',
-        clientSecret: '029aa5f1d0080ecb30e0867c60be88f5'
-    }
-}
